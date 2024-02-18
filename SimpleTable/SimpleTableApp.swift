4df48d33d7d9457e3d69836db5d7fe29893d9c3d
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2024-02-18 23:06:58 +0800

    Initial Commit

---
+++ SimpleTable/SimpleTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTableApp.swift
+//  SimpleTable
+//
+//  Created by donghs on 2024/2/18.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
